9f224d3309eb13f16967b26a8c151ee60b9d7f5f
Le Ngoc Doan <oneaicoder@gmail.com>
2026-04-19 11:35:33 +0700

    Fix syntax error in {{ModuleName}}Collection.php by adding missing semicolon in use statement

--- src/templates/module/Masks/{{ModuleName}}Collection.php
+++ src/templates/module/Masks/{{ModuleName}}Collection.php
@@ -1,7 +1,7 @@
 <?php
 namespace {{Namespace}}\Masks;
 use {{Namespace}}\Models\{{ModuleName}};
-use Saola\Core\Masks\MaskCollection
+use Saola\Core\Masks\MaskCollection;
 class {{ModuleName}}Collection extends MaskCollection 
 {
     protected $mask = {{ModuleName}}Mask::class;
