edafb3aa9f57562b4efda8f0927ca52a8b7b8a03
Le Ngoc Doan <oneaicoder@gmail.com>
2026-04-19 10:16:53 +0700

    Add ModuleServiceProvider class for backward compatibility and routing logic

---
+++ src/core/Providers/ModuleServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace Saola\Core\Providers;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+abstract class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Được giữ lại để tương thích ngược.
+     * Mọi logic đã được hợp nhất vào SaolaServiceProvider.
+     */
+    abstract function routes();
+
+    public function booting(Closure $callback)
+    {
+        if (method_exists($this, 'routes')) {
+            $this->routes();
+        }
+        return parent::booting($callback);
+    }
+}
